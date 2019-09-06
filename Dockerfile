e4ff3b0648fa5c48f2807801270bcf841af92122
Nicolas De Loof <nicolas.deloof@gmail.com>
2019-09-06 14:32:25 +0200

    Rebuild OSX SDK 10.11 as required by osxcross
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,14 @@ RUN     sed -ri "s/(httpredir|deb).debian.org/$APT_MIRROR/g" /etc/apt/sources.li
 RUN     apt-get update -qq && apt-get install -y -q --no-install-recommends \
             libltdl-dev \
             gcc-mingw-w64 \
-            parallel \
+            parallel \            
+            cmake \
+            libssl-dev \
+            lzma-dev \
+            libxml2-dev \
             ;
 
+COPY    MacOSX10.11.sdk.tar.xz /tmp/
 COPY    osx-cross.sh /tmp/
 RUN     /tmp/osx-cross.sh
 ENV     PATH /osxcross/target/bin:$PATH
--- osx-cross.sh
+++ osx-cross.sh
@@ -13,30 +13,30 @@ time apt-get install -y -q --no-install-recommends $PKG_DEPS
 
 # NOTE: when changing version here, make sure to
 # also change OSX_CODENAME below to match
-OSX_SDK=MacOSX10.10.sdk
-SDK_SUM=631b4144c6bf75bf7a4d480d685a9b5bda10ee8d03dbf0db829391e2ef858789
+OSX_SDK=MacOSX10.11.sdk
+SDK_SUM=694a66095a3514328e970b14978dc78c0f4d170e590fa7b2c3d3674b75f0b713
 
-OSX_CROSS_COMMIT=a9317c18a3a457ca0a657f08cc4d0d43c6cf8953
+OSX_CROSS_COMMIT=d39ba022313f2d5a1f5d02caaa1efb23d07a559b
 OSXCROSS_PATH="/osxcross"
 
 LIBTOOL_VERSION=2.4.6
-OSX_CODENAME=yosemite
+OSX_CODENAME=el_capitan
 
 echo "Cloning osxcross"
 time git clone https://github.com/tpoechtrager/osxcross.git $OSXCROSS_PATH
 cd $OSXCROSS_PATH
 git checkout -q $OSX_CROSS_COMMIT
 
-echo "Downloading OSX SDK"
-time curl -sSL https://s3.dockerproject.org/darwin/v2/${OSX_SDK}.tar.xz \
-    -o "${OSXCROSS_PATH}/tarballs/${OSX_SDK}.tar.xz"
+#echo "Downloading OSX SDK"
+#time curl -sSL https://s3.dockerproject.org/darwin/v2/${OSX_SDK}.tar.xz \
+mv /tmp/MacOSX10.11.sdk.tar.xz  "${OSXCROSS_PATH}/tarballs/${OSX_SDK}.tar.xz"
 
-echo "$SDK_SUM  ${OSXCROSS_PATH}/tarballs/${OSX_SDK}.tar.xz" \
-	| sha256sum -c -
+#echo "$SDK_SUM  ${OSXCROSS_PATH}/tarballs/${OSX_SDK}.tar.xz" \
+#	| sha256sum -c -
 
 echo "Building osxcross"
-# Go 1.13 requires OSX >= 10.11
-UNATTENDED=yes OSX_VERSION_MIN=10.11 ${OSXCROSS_PATH}/build.sh > /dev/null
+# Go 1.11 requires OSX >= 10.10
+UNATTENDED=yes OSX_VERSION_MIN=10.10 ${OSXCROSS_PATH}/build.sh > /dev/null
 
 echo "Installing libtool from brew"
 curl -sSL https://homebrew.bintray.com/bottles/libtool-${LIBTOOL_VERSION}.${OSX_CODENAME}.bottle.tar.gz \
